773c8693e7714ab1be899ff577f266542d99db0e
Matt <mattstates@icloud.com>
2018-09-12 20:35:53 -0700

    initial commit

---
+++ ClientApp/src/components/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Grid, Row } from 'react-bootstrap';
+import { NavMenu } from './NavMenu';
+import './Layout.css';
+
+export class Layout extends Component {
+    displayName = Layout.name;
+
+    render() {
+        return (
+            <Grid fluid>
+                <Row>
+                    <NavMenu />
+
+                    {this.props.children}
+                </Row>
+            </Grid>
+        );
+    }
+}
