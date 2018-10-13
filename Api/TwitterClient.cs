8edff43c5f839237c82c9eb7b005b14355429647
Matt <mattstates@icloud.com>
2018-10-13 10:55:14 -0700

    refactoring for twitter

--- Api/TwitterClient.cs
+++ Api/TwitterClient.cs
@@ -6,68 +6,113 @@ using System.Security.Cryptography;
 using System.Text;
 using System;
 
+// Adapted from: https://www.codeproject.com/Articles/1200390/Taming-the-Twitter-API-in-Csharp
 namespace WebUI.Code
 {
     public class TwitterClient
     {
-        public const string OauthVersion = "1.0";
-        public const string OauthSignatureMethod = "HMAC-SHA1";
+        public const string OAUTH_VERSION = "1.0";
+        public const string OAUTH_SIGNATURE_METHOD = "HMAC-SHA1";
+        public const string BASE_TWITTER_PATH = "https://api.twitter.com/1.1/statuses/";
+        private string _consumerKey;
+        private string _consumerKeySecret;
+        private string _accessToken;
+        private string _accessTokenSecret;
 
-        public TwitterClient (string consumerKey, string consumerKeySecret, string accessToken, string accessTokenSecret)
+        public TwitterClient(string consumerKey, string consumerKeySecret, string accessToken, string accessTokenSecret)
         {
-            this.ConsumerKey = consumerKey;
-            this.ConsumerKeySecret = consumerKeySecret;
-            this.AccessToken = accessToken;
-            this.AccessTokenSecret = accessTokenSecret;
+            _consumerKey = consumerKey;
+            _consumerKeySecret = consumerKeySecret;
+            _accessToken = accessToken;
+            _accessTokenSecret = accessTokenSecret;
         }
 
-        public string ConsumerKey { set; get; }
-        public string ConsumerKeySecret { set; get; }
-        public string AccessToken { set; get; }
-        public string AccessTokenSecret { set; get; }
+        // public string GetMentions(int count)
+        // {
+        //     string resourceUrl =
+        //         string.Format("http://api.twitter.com/1/statuses/mentions.json");
 
-        public string GetMentions(int count)
-        {
-            string resourceUrl =
-                string.Format("http://api.twitter.com/1/statuses/mentions.json");
+        //     var requestParameters = new SortedDictionary<string, string>();
+        //     requestParameters.Add("count", count.ToString());
+        //     requestParameters.Add("include_entities", "true");
 
-            var requestParameters = new SortedDictionary<string, string>();
-            requestParameters.Add("count", count.ToString());
-            requestParameters.Add("include_entities", "true");
+        //     var response = GetResponse(resourceUrl, Method.GET, requestParameters);
 
-            var response = GetResponse(resourceUrl, Method.GET, requestParameters);
-
-            return response;
-        }
+        //     return response;
+        // }
 
         public string GetTweets(string screenName, int count)
         {
-            string resourceUrl =
-                string.Format("https://api.twitter.com/1.1/statuses/user_timeline.json");
-
             var requestParameters = new SortedDictionary<string, string>();
             requestParameters.Add("count", count.ToString());
             requestParameters.Add("screen_name", screenName);
 
-            var response = GetResponse(resourceUrl, Method.GET, requestParameters);
+            var response = GetResponse($"{BASE_TWITTER_PATH}user_timeline.json", Method.GET, requestParameters);
 
             return response;
         }
 
-        public string PostStatusUpdate(string status, double latitude, double longitude)
-        {
-            const string resourceUrl = "http://api.twitter.com/1/statuses/update.json";
+        // public string PostStatusUpdate(string status, double latitude, double longitude)
+        // {
+        //     const string resourceUrl = "http://api.twitter.com/1/statuses/update.json";
 
-            var requestParameters = new SortedDictionary<string, string>();
-            requestParameters.Add("status", status);
-            requestParameters.Add("lat", latitude.ToString());
-            requestParameters.Add("long", longitude.ToString());
+        //     var requestParameters = new SortedDictionary<string, string>();
+        //     requestParameters.Add("status", status);
+        //     requestParameters.Add("lat", latitude.ToString());
+        //     requestParameters.Add("long", longitude.ToString());
 
-            return GetResponse(resourceUrl, Method.POST, requestParameters);
-        }
+        //     return GetResponse(resourceUrl, Method.POST, requestParameters);
+        // }
 
-        private string GetResponse
-        (string resourceUrl, Method method, SortedDictionary<string, string> requestParameters)
+        // private string GetResponse
+        // (string resourceUrl, Method method, SortedDictionary<string, string> requestParameters)
+        // {
+        //     ServicePointManager.Expect100Continue = false;
+        //     WebRequest request = null;
+        //     string resultString = string.Empty;
+
+        //     if (method == Method.POST)
+        //     {
+        //         var postBody = requestParameters.ToWebString();
+
+        //         request = (HttpWebRequest)WebRequest.Create(resourceUrl);
+        //         request.Method = method.ToString();
+        //         request.ContentType = "application/x-www-form-urlencoded";
+
+        //         using (var stream = request.GetRequestStream())
+        //         {
+        //             byte[] content = Encoding.ASCII.GetBytes(postBody);
+        //             stream.Write(content, 0, content.Length);
+        //         }
+        //     }
+        //     else if (method == Method.GET)
+        //     {
+        //         request = (HttpWebRequest)WebRequest.Create(resourceUrl + "?"
+        //             + requestParameters.ToWebString());
+        //         request.Method = method.ToString();
+        //     }
+        //     else
+        //     {
+        //         //other verbs can be addressed here...
+        //     }
+
+        //     if (request != null)
+        //     {
+        //         var authHeader = CreateHeader(resourceUrl, method, requestParameters);
+        //         request.Headers.Add("Authorization", authHeader);
+        //         var response = request.GetResponse();
+
+        //         using (var sd = new StreamReader(response.GetResponseStream()))
+        //         {
+        //             resultString = sd.ReadToEnd();
+        //             response.Close();
+        //         }
+        //     }
+
+        //     return resultString;
+        // }
+
+        private string GetResponse(string url, Method method, SortedDictionary<string, string> parameters)
         {
             ServicePointManager.Expect100Continue = false;
             WebRequest request = null;
@@ -75,9 +120,9 @@ namespace WebUI.Code
 
             if (method == Method.POST)
             {
-                var postBody = requestParameters.ToWebString();
+                var postBody = parameters.ToWebString();
 
-                request = (HttpWebRequest) WebRequest.Create(resourceUrl);
+                request = (HttpWebRequest)WebRequest.Create(url);
                 request.Method = method.ToString();
                 request.ContentType = "application/x-www-form-urlencoded";
 
@@ -89,8 +134,8 @@ namespace WebUI.Code
             }
             else if (method == Method.GET)
             {
-                request = (HttpWebRequest)WebRequest.Create(resourceUrl + "?" 
-                    + requestParameters.ToWebString());
+                request = (HttpWebRequest)WebRequest.Create(url + "?"
+                    + parameters.ToWebString());
                 request.Method = method.ToString();
             }
             else
@@ -100,7 +145,7 @@ namespace WebUI.Code
 
             if (request != null)
             {
-                var authHeader = CreateHeader(resourceUrl, method, requestParameters);
+                var authHeader = CreateHeader(url, method, parameters);
                 request.Headers.Add("Authorization", authHeader);
                 var response = request.GetResponse();
 
@@ -114,19 +159,17 @@ namespace WebUI.Code
             return resultString;
         }
 
-        private string CreateOauthNonce()
+        private static string CreateOauthNonce()
         {
             return Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
         }
 
-        private string CreateHeader(string resourceUrl, Method method,
-                                    SortedDictionary<string, string> requestParameters)
+        private string CreateHeader(string url, Method method, SortedDictionary<string, string> parameters)
         {
             var oauthNonce = CreateOauthNonce();
-                // Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+            // Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
             var oauthTimestamp = CreateOAuthTimestamp();
-            var oauthSignature = CreateOauthSignature
-            (resourceUrl, method, oauthNonce, oauthTimestamp, requestParameters);
+            var oauthSignature = CreateOauthSignature(url, method, oauthNonce, oauthTimestamp, parameters);
 
             //The oAuth signature is then used to generate the Authentication header. 
             const string headerFormat = "OAuth oauth_nonce=\"{0}\", " +
@@ -139,45 +182,37 @@ namespace WebUI.Code
 
             var authHeader = string.Format(headerFormat,
                                            Uri.EscapeDataString(oauthNonce),
-                                           Uri.EscapeDataString(OauthSignatureMethod),
+                                           Uri.EscapeDataString(OAUTH_SIGNATURE_METHOD),
                                            Uri.EscapeDataString(oauthTimestamp),
-                                           Uri.EscapeDataString(ConsumerKey),
-                                           Uri.EscapeDataString(AccessToken),
+                                           Uri.EscapeDataString(_consumerKey),
+                                           Uri.EscapeDataString(_accessToken),
                                            Uri.EscapeDataString(oauthSignature),
-                                           Uri.EscapeDataString(OauthVersion)
-                );
+                                           Uri.EscapeDataString(OAUTH_VERSION));
 
             return authHeader;
         }
 
-        private string CreateOauthSignature
-        (string resourceUrl, Method method, string oauthNonce, string oauthTimestamp,
-                                            SortedDictionary<string, string> requestParameters)
+        private string CreateOauthSignature(string url, Method method, string nonce, string timestamp, SortedDictionary<string, string> parameters)
         {
             //firstly we need to add the standard oauth parameters to the sorted list
-            requestParameters.Add("oauth_consumer_key", ConsumerKey);
-            requestParameters.Add("oauth_nonce", oauthNonce);
-            requestParameters.Add("oauth_signature_method", OauthSignatureMethod);
-            requestParameters.Add("oauth_timestamp", oauthTimestamp);
-            requestParameters.Add("oauth_token", AccessToken);
-            requestParameters.Add("oauth_version", OauthVersion);
+            parameters.Add("oauth_consumer_key", _consumerKey);
+            parameters.Add("oauth_nonce", nonce);
+            parameters.Add("oauth_signature_method", OAUTH_SIGNATURE_METHOD);
+            parameters.Add("oauth_timestamp", timestamp);
+            parameters.Add("oauth_token", _accessToken);
+            parameters.Add("oauth_version", OAUTH_VERSION);
 
-            var sigBaseString = requestParameters.ToWebString();
+            var sigBaseString = parameters.ToWebString();
 
-            var signatureBaseString = string.Concat
-            (method.ToString(), "&", Uri.EscapeDataString(resourceUrl), "&",
-                                Uri.EscapeDataString(sigBaseString.ToString()));
+            var signatureBaseString = $"{method.ToString()}&{Uri.EscapeDataString(url)}&{Uri.EscapeDataString(sigBaseString.ToString())}";
 
-            //Using this base string, we then encrypt the data using a composite of the 
-            //secret keys and the HMAC-SHA1 algorithm.
-            var compositeKey = string.Concat(Uri.EscapeDataString(ConsumerKeySecret), "&",
-                                             Uri.EscapeDataString(AccessTokenSecret));
+            //Using this base string, we then encrypt the data using a composite of the secret keys and the HMAC-SHA1 algorithm.
+            var compositeKey = $"{Uri.EscapeDataString(_consumerKeySecret)}&{Uri.EscapeDataString(_accessTokenSecret)}";
 
             string oauthSignature;
             using (var hasher = new HMACSHA1(Encoding.ASCII.GetBytes(compositeKey)))
             {
-                oauthSignature = Convert.ToBase64String(
-                    hasher.ComputeHash(Encoding.ASCII.GetBytes(signatureBaseString)));
+                oauthSignature = Convert.ToBase64String(hasher.ComputeHash(Encoding.ASCII.GetBytes(signatureBaseString)));
             }
 
             return oauthSignature;
@@ -185,7 +220,6 @@ namespace WebUI.Code
 
         private static string CreateOAuthTimestamp()
         {
-
             var nowUtc = DateTime.UtcNow;
             var timeSpan = nowUtc - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             var timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
