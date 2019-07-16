bf983431a8c9b7f1eecee78ca48fdcc98cec3771
Matt <mattstates@icloud.com>
2019-07-16 13:09:49 -0700

    move console log to index

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Home } from './components/Home';
 import { Layout } from './components/Layout';
 import { Route } from 'react-router';
-import { CONSOLE_TITLE_STYLE, CONSOLE_BODY_STYLE } from './constants/styles.js';
-import { EMAIL_ADDRESS } from './constants/index.js';
 
 export default function App() {
     return (
@@ -12,10 +10,3 @@ export default function App() {
         </Layout>
     );
 }
-
-console.log('%cMatt States', CONSOLE_TITLE_STYLE);
-console.log(
-    `%cThanks for inspecting my site. Feel free to reach out at:\n ${EMAIL_ADDRESS}`,
-    CONSOLE_BODY_STYLE
-);
-console.log('%c ', CONSOLE_TITLE_STYLE);
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,10 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { CONSOLE_TITLE_STYLE, CONSOLE_BODY_STYLE } from './constants/styles.js';
+import { EMAIL_ADDRESS } from './constants/index.js';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -14,3 +16,10 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   rootElement);
+
+  console.log('%cMatt States', CONSOLE_TITLE_STYLE);
+  console.log(
+      `%cThanks for inspecting my site. Feel free to reach out at:\n ${EMAIL_ADDRESS}`,
+      CONSOLE_BODY_STYLE
+  );
+  console.log('%c ', CONSOLE_TITLE_STYLE);
